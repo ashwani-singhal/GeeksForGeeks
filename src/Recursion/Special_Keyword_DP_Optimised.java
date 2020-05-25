4eac7eef0e21e985b838e93fd362a5fd0e4e293e
ashwani-singhal <ashwani16081997@gmail.com>
2020-05-25 20:06:24 +0530

    Initial Commit

--- src/Recursion/Special_Keyword_DP_Optimised.java
+++ src/Recursion/Special_Keyword_DP_Optimised.java
@@ -1,5 +1,41 @@
 package Recursion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Special_Keyword_DP_Optimised {
 
+    private int Specil_Number(int n){
+        if(n <= 6){
+            return n;
+        }
+
+        int[] ar = new int[n+1];
+
+        for (int i = 1; i <=n ; i++) {
+            ar[i] = i;
+        }
+
+        for (int i = 7; i <= n ; i++) {
+            ar[i] = Math.max(2*ar[n-3],Math.max(3*ar[n-4],4*ar[n-5]));
+        }
+
+        return ar[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        Special_Keyword_DP_Optimised obj = new Special_Keyword_DP_Optimised();
+        int t = Integer.parseInt(buf.readLine());
+        while (t-- != 0){
+            int n = Integer.parseInt(buf.readLine());
+            if (n > 75){
+                System.out.println(-1);
+            }
+            else{
+                System.out.println(obj.Specil_Number(n));
+            }
+        }
+    }
 }
